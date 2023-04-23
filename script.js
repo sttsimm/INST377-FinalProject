1b8a4ebf3db1757218cbb281051614aa4bd4f0c2
sttsimm <stsimmer@terpmail.umd.edu>
2023-04-23 16:16:52 -0400

    update

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function getLightingTimes() {
     // store ZIP code and API parameters
     var zipCode = document.getElementById("zip").value;
     var url = "https://www.hebcal.com/shabbat?cfg=json&zip=" + zipCode + "&M=on";
+        // request
+        fetch(url)
+        .then(function(response) {
+          return response.json();
+        })
