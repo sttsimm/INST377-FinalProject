3fa9435c3deb782fde0ffffa09c7de22fc67cae7
sttsimm <stsimmer@terpmail.umd.edu>
2023-04-23 16:50:20 -0400

    add js async call

--- script.js
+++ script.js
@@ -7,3 +7,28 @@ function getLightingTimes() {
         .then(function(response) {
           return response.json();
         })
+
+        .then(function(data) {
+            // Filtering response data to extract Shabbat start times
+            var Times = data.items.filter(function(item) {
+              return item.category === "candles";
+            });
+
+            //results div
+            var results = document.getElementById("results");
+      
+            // Clear prev results from div
+            results.innerHTML = "";
+      
+            // loop through start times, add to results div
+            Times.forEach(function(time) {
+              var date = new Date(time.date);
+              var formatDate = date.toLocaleDateString();
+              var formatTime = time.title;
+              results.innerHTML += "<p>" + formatDate + ": " + formatTime + "</p>";
+            });
+          })
+          .catch(function(error) {
+            console.error(error);
+          });
+      }
